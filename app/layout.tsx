375f032ab71d234b33975b38bac2512003e50cfa
hoang khanh <khanh.tranhoang1999@gmail.com>
2024-02-29 22:46:46 +0700

    add tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from '@/components/Header';
-import '@/styles/app.scss'
+import './global.scss'
 
 export const metadata: Metadata = {
   title: "Upwork VN Version",
