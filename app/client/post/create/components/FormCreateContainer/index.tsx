687234d1ad2cb64dba677450e1028794aaedb036
hoang khanh <khanh.tranhoang1999@gmail.com>
2024-03-23 16:18:45 +0700

    push code

--- app/client/post/create/components/FormCreateContainer/index.tsx
+++ app/client/post/create/components/FormCreateContainer/index.tsx
@@ -1,19 +1,30 @@
 'use client';
+import React, { useContext, useState } from 'react';
+import { useForm, SubmitHandler, SubmitErrorHandler } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
-import { useContext, useMemo } from 'react';
-import { CreatePostContext } from '../../context/CreatePostContext';
-import StepOne from './StepOne';
-import StepTwo from './StepTwo';
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from '@/app/components/ui/select';
+import { cn } from '@/lib/utils';
+
 import {
     Form,
-    SubmitErrorHandler,
-    SubmitHandler,
-    useForm,
-} from 'react-hook-form';
-import * as yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
-
-interface ICreatePostData {}
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '@/app/components/ui/form';
+import { Input } from '@/app/components/ui/input';
+import { Button } from '@/app/components/ui/button';
+import Link from 'next/link';
+import { CreatePostContext } from '../../context/CreatePostContext';
 
 const createPostSchema = yup.object({
     title: yup
@@ -38,8 +49,11 @@ const createPostSchema = yup.object({
         .required('Vui lòng chọn ít nhất 1 skill'),
 });
 
+interface ICreatePostData {}
+
 const FormCreateContainer = () => {
     const { step } = useContext(CreatePostContext);
+
     const form = useForm({
         resolver: yupResolver(createPostSchema),
         defaultValues: {
@@ -50,7 +64,6 @@ const FormCreateContainer = () => {
             status: 0,
         },
     });
-
     const onSubmit: SubmitHandler<ICreatePostData> = (data) =>
         handleCreatePost(data);
 
@@ -66,7 +79,34 @@ const FormCreateContainer = () => {
         <section className='container px-20 py-8'>
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit, onError)}>
-                    {step === 1 && <StepOne />}
+                    {step === 1 && (
+                        <div className='grid grid-cols-2 gap-x-8 px-12 mx-12'>
+                            <div>
+                                <small className='mb-6 inline-block text-sm'>
+                                    {`1/5`}{' '}
+                                    <span className='ml-6'>Job post</span>
+                                </small>
+                                <h2 className='mb-6 text-4xl leading-8 -tracking-[0.25px] font-rza'>{`Let's start with a strong title and description.`}</h2>
+                                <p className='text-sm leading-5 mb-8'>
+                                    {`This helps your job post stand out to the right candidates. It’s the first thing they’ll see, so make it count!`}
+                                </p>
+                            </div>
+                            <div>
+                                <h5>Write a title for your job post</h5>
+                                <FormField
+                                    control={form.control}
+                                    name='title'
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <Input {...field} />
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
+                        </div>
+                    )}
                 </form>
             </Form>
         </section>
