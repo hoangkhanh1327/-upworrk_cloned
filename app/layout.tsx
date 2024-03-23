d02eb940f0753e1e7c28ead940c1ef7feff2b356
Huyen Tran <ttminhuyen1111@gmail.com>
2024-03-23 16:05:27 +0700

    context

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,27 @@
-import type { Metadata } from 'next';
-import './global.css';
-import AuthProvider from './providers/AuthProvider';
+import type { Metadata } from "next";
+import "./global.css";
+import AuthProvider from "./providers/AuthProvider";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+
 
 export const metadata: Metadata = {
-    title: 'IT WORKS VN Version',
-    description: 'IT WORKS',
+  title: "IT WORKS VN Version",
+  description: "IT WORKS",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang='en'>
-            <body>
-                <AuthProvider>{children}</AuthProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body>
+        {/* Wrap AuthProvider with ThirdWebProvider */}
+        <AuthProvider>
+          <ThirdwebProvider>{children}</ThirdwebProvider>
+        </AuthProvider>
+      </body>
+    </html>
+  );
 }
