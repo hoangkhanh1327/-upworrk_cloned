46157a1915c5df774b9bd19800e14a4bb162f278
hoang khanh <khanh.tranhoang1999@gmail.com>
2024-03-23 16:48:52 +0700

    push cocde

--- app/client/post/create/components/FormCreateContainer/index.tsx
+++ app/client/post/create/components/FormCreateContainer/index.tsx
@@ -25,6 +25,7 @@ import { Input } from '@/app/components/ui/input';
 import { Button } from '@/app/components/ui/button';
 import Link from 'next/link';
 import { CreatePostContext } from '../../context/CreatePostContext';
+import { Textarea } from '@/app/components/ui/textarea';
 
 const createPostSchema = yup.object({
     title: yup
@@ -92,14 +93,73 @@ const FormCreateContainer = () => {
                                 </p>
                             </div>
                             <div>
-                                <h5>Write a title for your job post</h5>
                                 <FormField
                                     control={form.control}
                                     name='title'
                                     render={({ field }) => (
-                                        <FormItem>
+                                        <FormItem className='mb-6'>
+                                            <FormLabel>
+                                                Write a title for your job post
+                                            </FormLabel>
                                             <FormControl>
                                                 <Input {...field} />
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name='desc'
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>
+                                                Write a short description about
+                                                your job post
+                                            </FormLabel>
+                                            <FormControl>
+                                                <Textarea {...field} />
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
+                        </div>
+                    )}
+                    {step === 2 && (
+                        <div className='grid grid-cols-2 gap-x-8 px-12 mx-12'>
+                            <div>
+                                <small className='mb-6 inline-block text-sm'>
+                                    {`2/5`}{' '}
+                                    <span className='ml-6'>Job post</span>
+                                </small>
+                                <h2 className='mb-6 text-4xl leading-8 -tracking-[0.25px] font-rza'>{`What are the main skills required for your work?`}</h2>
+                            </div>
+                            <div>
+                                <FormField
+                                    control={form.control}
+                                    name='title'
+                                    render={({ field }) => (
+                                        <FormItem className='mb-6'>
+                                            <FormLabel>
+                                                Write a title for your job post
+                                            </FormLabel>
+                                            <FormControl>
+                                                <Input {...field} />
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name='desc'
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>
+                                                Write a short description about
+                                                your job post
+                                            </FormLabel>
+                                            <FormControl>
+                                                <Textarea {...field} />
                                             </FormControl>
                                         </FormItem>
                                     )}
