9a442689de67fc8072f2ceb6ef10403a99118641
hoang khanh <khanh.tranhoang1999@gmail.com>
2024-03-25 19:51:14 +0700

    add dropdown user

--- app/components/ProtectedHeader/UserDropdown.tsx
+++ app/components/ProtectedHeader/UserDropdown.tsx
@@ -1,48 +1,47 @@
-import { DropdownMenuCheckboxItemProps } from '@radix-ui/react-dropdown-menu';
-import { useState } from 'react';
 import {
     DropdownMenu,
-    DropdownMenuCheckboxItem,
     DropdownMenuContent,
+    DropdownMenuItem,
     DropdownMenuLabel,
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from '../ui/dropdown-menu';
+import { CircleUserRound, LogOut } from 'lucide-react';
+import { useAuth } from '@/app/providers/AuthProvider';
 import { Button } from '../ui/button';
 
-type Checked = DropdownMenuCheckboxItemProps['checked'];
 const UserDropdown = () => {
-    const [showStatusBar, setShowStatusBar] = useState<Checked>(true);
-    const [showActivityBar, setShowActivityBar] = useState<Checked>(false);
-    const [showPanel, setShowPanel] = useState<Checked>(false);
+    const { user, logout } = useAuth();
 
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button variant='outline'>Open</Button>
+                <CircleUserRound className='ml-3 cursor-pointer w-7 h-7' />
             </DropdownMenuTrigger>
-            <DropdownMenuContent className='w-56'>
-                <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+            <DropdownMenuContent className='w-60'>
+                <DropdownMenuItem>
+                    <div className='flex flex-col items-center justify-center w-full py-5'>
+                        <CircleUserRound className='w-20 h-20' />
+                        <h4 className='mt-2.5 text-center text-base font-medium'>
+                            {user?.first_name
+                                ? `${user?.first_name} ${user?.last_name}`
+                                : user?.username}
+                        </h4>
+                        <small></small>
+                    </div>
+                </DropdownMenuItem>
                 <DropdownMenuSeparator />
-                <DropdownMenuCheckboxItem
-                    checked={showStatusBar}
-                    onCheckedChange={setShowStatusBar}
-                >
-                    Status Bar
-                </DropdownMenuCheckboxItem>
-                <DropdownMenuCheckboxItem
-                    checked={showActivityBar}
-                    onCheckedChange={setShowActivityBar}
-                    disabled
-                >
-                    Activity Bar
-                </DropdownMenuCheckboxItem>
-                <DropdownMenuCheckboxItem
-                    checked={showPanel}
-                    onCheckedChange={setShowPanel}
-                >
-                    Panel
-                </DropdownMenuCheckboxItem>
+                <DropdownMenuItem onClick={logout}>
+                    <DropdownMenuLabel asChild>
+                        <Button
+                            className='w-full border-none bg-transparent text-left justify-between'
+                            variant='outline'
+                        >
+                            <span>Đăng Xuất</span>
+                            <LogOut />
+                        </Button>
+                    </DropdownMenuLabel>
+                </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
     );
