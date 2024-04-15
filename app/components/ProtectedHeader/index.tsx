578f00305e1a6660a7337afe100a76a83834bfad
hoang khanh <khanh.tranhoang1999@gmail.com>
2024-04-15 08:24:13 +0700

    add fixed header

--- app/components/ProtectedHeader/index.tsx
+++ app/components/ProtectedHeader/index.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 
 import Navbar from './Navbar';
 import Logo from './Logo';
 import Action from './Action';
+import { cn } from '@/lib/utils';
 
 const Header = () => {
+    const [fixed, setFixed] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY >= 69) {
+                setFixed(true);
+            } else {
+                setFixed(false);
+            }
+        };
+        document.addEventListener('scroll', handleScroll);
+        return () => {
+            document.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <div className=''>
+        <div
+            className={cn(
+                '',
+                fixed ? 'fixed top-0 left-0 right-0 bg-white z-50 shadow-xl' : 'relative'
+            )}
+        >
             <header className='container'>
                 <div className='h-16 flex items-center'>
                     <Logo />
